f06ffb6de2b44c252f9f357d49bd3f578e5e7840
yanick.salzmann <yanick.salzmann@gmail.com>
2019-07-28 18:13:28 +0200

    Logger demangling

--- utils/src/Log.cpp
+++ utils/src/Log.cpp
@@ -1,6 +1,39 @@
 #include "utils/Log.h"
 #include <spdlog/sinks/stdout_color_sinks.h>
 
+#ifdef __GNUG__
+#include <cstdlib>
+#include <memory>
+#include <cxxabi.h>
+
+std::string demangle(const char* name) {
+
+    int status = -4; // some arbitrary value to eliminate the compiler warning
+
+    // enable c++11 by passing the flag -std=c++11 to g++
+    std::unique_ptr<char, void(*)(void*)> res {
+            abi::__cxa_demangle(name, NULL, NULL, &status),
+            std::free
+    };
+
+    return (status==0) ? res.get() : name ;
+}
+
+#else
+#include <dbghelp.h>
+
+std::string demangle(const char* name) {
+    char undecorated_name[5000]{};
+    if(!UnDecorateSymbolName(name, undecorated_name, sizeof(undecorated_name), UNDNAME_COMPLETE)) {
+        return name;
+    }
+
+    undecorated_name[4999] = '\0';
+    return undecorated_name;
+}
+
+#endif
+
 namespace message::utils {
     void Logger::init_logger (const std::string& name) {
         _logger = spdlog::stdout_color_mt (name);
@@ -13,11 +46,13 @@ namespace message::utils {
     }
 
     std::string Logger::sanitize_type_name(const std::string &name) {
-        const auto idx_space = name.find(' ');
+        auto actual_name = demangle(name.c_str());
+
+        const auto idx_space = actual_name.find(' ');
         if(idx_space == std::string::npos) {
-            return name;
+            return actual_name;
         }
 
-        return name.substr(idx_space + 1);
+        return actual_name.substr(idx_space + 1);
     }
 }
